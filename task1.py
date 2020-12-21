d8b1e3788afd613b87c55534e69b8cceafbac127
Dev <voron12121@gmail.com>
2020-12-22 00:14:14 +0300

    Task1

---
+++ task1.py
@@ -0,0 +1,10 @@
+from files import get_files_count
+from file import FileProcessing
+from file import list_sorting
+
+
+def main():
+    fp = FileProcessing
+    print(f'Task 1: count files\n{get_files_count("C:/Users/Dev1100/Desktop/Python/laba/12") = }')
+if __name__ == '__main__':
+    main()
