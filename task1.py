4a2c10bd40607bdb39e986f4dc7308eb1ceee777
Dev <voron12121@gmail.com>
2020-12-22 00:27:43 +0300

    Task2.3

--- task1.py
+++ task1.py
@@ -10,6 +10,7 @@ def main():
     data = fp.make_list_from_file("students.csv")
     print(f'Task 2.1: list sorting by last name\n{list_sorting(data[1:], 1) = }')
     print(f'Task 2.2: age > 22\n{[x for x in data[1:] if int(x[2]) >= 22] = }')
+    print(f'Task 2.3: writing in csv\n{fp.write_csv(data, "test.csv") = }')
 
 if __name__ == '__main__':
     main()
