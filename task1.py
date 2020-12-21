f37327757bbc6d3e7923052ae1424d43660f8daa
Dev <voron12121@gmail.com>
2020-12-22 00:16:27 +0300

    Task2.1

--- task1.py
+++ task1.py
@@ -7,6 +7,8 @@ def main():
     fp = FileProcessing
     print(f'Task 1: count files\n{get_files_count("D:/pe_lab4") = }')
     print(f'Task 2: making list from file\n{fp.make_list_from_file("students.csv") = }')
+    data = fp.make_list_from_file("students.csv")
+    print(f'Task 2.1: list sorting by last name\n{list_sorting(data[1:], 1) = }')
 
 if __name__ == '__main__':
     main()
